2ea151c25d958cd500e780e3d4fde6584ca89d52
Akash Raj <126938815+akashrajayush@users.noreply.github.com>
2024-12-30 23:19:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-console.log("Main.js working")
+console.log("Main.js working");
 
 const populate = async (value, currency) => {
-    let myStr = ""
-    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=" + currency
-    let response = await fetch(url)
-    let rJson = await response.json()
-    document.querySelector(".output").style.display = "block"
+    const url = `https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=${currency}`;
+    const response = await fetch(url);
+    const rJson = await response.json();
+    document.querySelector(".output").style.display = "block";
 
-    for (let key of Object.keys(rJson["data"])) {
-        myStr += ` <tr>
-                        <td>${key}</td>
-                        <td>${rJson["data"][key]["code"]}</td>
-                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
-                    </tr> 
-                `
+    let tableContent = "";
+    for (const [key, data] of Object.entries(rJson.data)) {
+        tableContent += `
+            <tr>
+                <td>${key}</td>
+                <td>${data.code}</td>
+                <td>${Math.round(data.value * value)}</td>
+            </tr>
+        `;
     }
-    const tableBody = document.querySelector("tbody");
-    tableBody.innerHTML = myStr;
 
-}
-const btn = document.querySelector(".btn")
-btn.addEventListener("click", (e) => {
-    e.preventDefault()
+    document.querySelector("tbody").innerHTML = tableContent;
+};
+
+document.querySelector("#converter-form").addEventListener("submit", (e) => {
+    e.preventDefault();
     const value = parseInt(document.querySelector("input[name='quantity']").value);
-    const currency = document.querySelector("select[name='currency']").value
-    populate(value, currency)
-})
+    const currency = document.querySelector("select[name='currency']").value;
+    populate(value, currency);
+});
