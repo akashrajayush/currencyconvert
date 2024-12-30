7624834c805fd831d754aeab4663b4ec3e3d989a
Akash Raj <126938815+akashrajayush@users.noreply.github.com>
2024-12-30 23:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ console.log("script.js working")
 const populate = async (value, currency) => {
     let myStr = ""
     url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=" + currency
-
     let response = await fetch(url)
     let rJson = await response.json()
     document.querySelector(".output").style.display = "block"
